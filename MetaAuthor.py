35cff29afd6057058f0daa0a91b992d5666a2e4f
Samwel Opiyo <samonyango20@gmail.com>
2017-03-16 13:36:06 +0300

    Added Author Meta Description

--- MetaAuthor.py
+++ MetaAuthor.py
@@ -2,20 +2,19 @@ import requests
 from bs4 import BeautifulSoup as soup
 import urlparse
 
-def scraping():
+def scraping(a):
     url = raw_input("Enter a valid url link: ")
     if urlparse.urlsplit(url)[0] != "http" or urlparse.urlsplit(url)[0] != "https":
         url = "http://" + url
-    get = requests.get(url)
-    read = soup(get.text)
-    return read.select("meta")
+ 
+    return soup(requests.get(url).text, "lxml").select(a)
 
 def choice():
     choice_input = raw_input("Enter Author to get Author Description or All to get all Meta Description :")
     if choice_input == "All" or choice_input == "all":
-        print scraping()
+        print scraping("meta")
     else:
-        print scraping(), "Author"
+        print scraping('meta[name="author"]')
 
 choice()
 
